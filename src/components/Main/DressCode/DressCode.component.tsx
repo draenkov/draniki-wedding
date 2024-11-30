5dc77fe4011f0702e1cf4622ec6f8d75ac647c81
draenkov <drayankou@gmail.com>
2024-11-30 22:46:14 +0300

    add parallax

--- src/components/Main/DressCode/DressCode.component.tsx
+++ src/components/Main/DressCode/DressCode.component.tsx
@@ -1,29 +1,74 @@
-import React, { FC } from 'react';
+'use client';
+
+import React, { FC, useRef } from 'react';
 import styles from 'components/Main/DressCode/DressCode.module.scss';
+import { motion } from 'motion/react';
+import useDeviceDetect from 'hooks/useDeviceDetect';
+import { useParallax } from 'hooks/useParallax';
 
-const colors = ['#252525', '#2e3458', '#828956', '#c16036', '#fff7d2'];
+const START_POSITION = 200;
+const START_POSITION_MOBILE = 100;
+const MOTION_END1 = 0.2;
+const MOTION_END2 = 0.3;
+const MOTION_END3 = 0.4;
+const MOTION_END4 = 0.5;
+const MOTION_END5 = 0.6;
 
-const DressCode: FC = () => (
-    <div className={styles.wrap}>
-        <div className={styles.container}>
-            <h3 className={styles.title}>Дресс-код</h3>
-            <div className={styles.dressCode}>
-                <p>
-                    Если вам сложно определиться с нарядом на праздник, то вы можете поддержать
-                    цветовую гамму нашей свадьбы
-                </p>
-                <div className={styles.colorsWrap}>
-                    {colors.map(colorItem => (
-                        <div
-                            className={styles.color}
-                            key={colorItem}
-                            style={{ backgroundColor: colorItem }}
-                        />
-                    ))}
+const DressCode: FC = () => {
+    const isMobile = useDeviceDetect() === 'mobile';
+    const scrollRef = useRef(null);
+
+    const x1 = useParallax(scrollRef, MOTION_END1, [
+        isMobile ? START_POSITION_MOBILE : START_POSITION,
+        0,
+    ]);
+    const x2 = useParallax(scrollRef, MOTION_END2, [
+        isMobile ? START_POSITION_MOBILE : START_POSITION,
+        0,
+    ]);
+    const x3 = useParallax(scrollRef, MOTION_END3, [
+        isMobile ? START_POSITION_MOBILE : START_POSITION,
+        0,
+    ]);
+    const x4 = useParallax(scrollRef, MOTION_END4, [
+        isMobile ? START_POSITION_MOBILE : START_POSITION,
+        0,
+    ]);
+    const x5 = useParallax(scrollRef, MOTION_END5, [
+        isMobile ? START_POSITION_MOBILE : START_POSITION,
+        0,
+    ]);
+
+    const colors = [
+        { backgroundColor: '#252525', x: x1 },
+        { backgroundColor: '#2e3458', x: x2 },
+        { backgroundColor: '#828956', x: x3 },
+        { backgroundColor: '#c16036', x: x4 },
+        { backgroundColor: '#fff7d2', x: x5 },
+    ];
+
+    return (
+        <div className={styles.wrap} ref={scrollRef}>
+            <div className={styles.container}>
+                <h3 className={styles.title}>Дресс-код</h3>
+                <div className={styles.dressCode}>
+                    <p>
+                        Если вам сложно определиться с нарядом на праздник, то вы можете поддержать
+                        цветовую гамму нашей свадьбы
+                    </p>
+                    <div className={styles.colorsWrap}>
+                        {colors.map(({ backgroundColor, x }) => (
+                            <motion.div
+                                className={styles.color}
+                                key={backgroundColor}
+                                style={{ backgroundColor, x }}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default DressCode;
--- src/components/Main/Info/Info.component.tsx
+++ src/components/Main/Info/Info.component.tsx
@@ -4,27 +4,27 @@ import React, { FC, useRef } from 'react';
 import styles from 'components/Main/Info/Info.module.scss';
 import Image from 'next/image';
 import photo from 'assets/img/photo2.jpg';
-import { motion, useScroll, useTransform } from 'motion/react';
+import { motion } from 'motion/react';
 import useDeviceDetect from 'hooks/useDeviceDetect';
+import { useParallax } from 'hooks/useParallax';
 
-const START_POSITION = 200;
-const START_POSITION_MOBILE = 100;
-const MOTION_END = 0.2;
+const START_POSITION = 40;
+const START_POSITION_MOBILE = 20;
+const MOTION_END = 0.4;
 
 const Info: FC = () => {
-    const isMobile = useDeviceDetect() === 'mobile';
     const scrollRef = useRef(null);
-    const { scrollYProgress } = useScroll();
-    const opacity = useTransform(scrollYProgress, [0, MOTION_END], [0, 1]);
-    const y = useTransform(
-        scrollYProgress,
-        [0, MOTION_END],
-        [isMobile ? START_POSITION_MOBILE : START_POSITION, 0],
-    );
+    const isMobile = useDeviceDetect() === 'mobile';
+
+    const opacity = useParallax(scrollRef, MOTION_END, [0, 1]);
+    const y = useParallax(scrollRef, MOTION_END, [
+        isMobile ? START_POSITION_MOBILE : START_POSITION,
+        0,
+    ]);
 
     return (
-        <motion.div className={styles.wrap} ref={scrollRef} style={{ opacity, y }}>
-            <div className={styles.info}>
+        <div className={styles.wrap}>
+            <motion.div className={styles.info} style={{ opacity, y }} ref={scrollRef}>
                 <div className={styles.infoItem}>
                     <p className={styles.title}>Когда?</p>
                     <p>12 июля 2025</p>
@@ -33,11 +33,11 @@ const Info: FC = () => {
                     <p className={styles.title}>Где?</p>
                     <p>г. Гродно</p>
                 </div>
-            </div>
+            </motion.div>
             <div className={styles.photo}>
                 <Image src={photo} alt="Второе лучшее фото молодых" />
             </div>
-        </motion.div>
+        </div>
     );
 };
 
